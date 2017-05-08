b20becca884e2dfeba2cd343991d4e9c083cb29c
Kento Hoshi <kento_hoshi@rish.kyoto-u.ac.jp>
2017-05-09 08:08:59 +0900

    Added init.fish.

---
+++ init.fish
@@ -0,0 +1,22 @@
+set fish_theme default
+set -U EDITOR vim
+
+alias ls 'ls -aF'
+alias ll 'ls -alh'
+
+switch (uname)
+    case Darwin
+        alias ls 'ls -aFG'
+        alias ll 'ls -alhG'
+    case '*'
+        alias ls 'ls -aF --color=always'
+        alias ll 'ls -alh --color=always'
+end
+
+alias gst 'git status'
+alias .. "cd .."
+alias :q "exit"
+alias less "less -R"
+alias diff "diff -y --suppress-common-lines"
+alias grep "grep -n --color=auto"
+alias mv "mv -v"
